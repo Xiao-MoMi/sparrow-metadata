a289e871c5520094261a5418d0f8e2caf89549f5
XiaoMoMi <littlemomi@qq.com>
2026-01-16 02:52:37 +0800

    1.11

--- core/src/main/java/net/momirealms/sparrow/metadata/ByteMetaDataValue.java
+++ core/src/main/java/net/momirealms/sparrow/metadata/ByteMetaDataValue.java
@@ -18,12 +18,20 @@ public class ByteMetaDataValue extends CommonMetaDataValue<Byte> implements Nume
         return Optional.ofNullable(getCachedValue()).map(it -> it == 1).orElse(null);
     }
 
-    public boolean getCachedValueAsBoolean(boolean defaultValue) {
+    public boolean getCachedValueAsBooleanOrDefault(boolean defaultValue) {
         return Optional.ofNullable(getCachedValue()).map(it -> it == 1).orElse(defaultValue);
     }
 
-    public boolean getCachedValueAsBoolean(Supplier<Boolean> defaultValue) {
+    public boolean getCachedValueAsBooleanOrDefault(Supplier<Boolean> defaultValue) {
         return Optional.ofNullable(getCachedValue()).map(it -> it == 1).orElseGet(defaultValue);
+    }
+
+    public CompletableFuture<Boolean> getAsBooleanOrDefault(boolean defaultValue) {
+        return get().thenApply(value -> value == null ? defaultValue : value != 0);
+    }
+
+    public CompletableFuture<Boolean> getAsBooleanOrDefault(Supplier<Boolean> defaultValue) {
+        return get().thenApply(value -> value == null ? defaultValue.get() : value != 0);
     }
 
     public CompletableFuture<Boolean> update(Boolean value) {
